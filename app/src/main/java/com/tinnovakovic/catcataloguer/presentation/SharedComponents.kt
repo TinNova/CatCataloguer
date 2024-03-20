77a8ab68b891af452ae5264d7f1cfe20bc8f9c5e
Tin Novakovic <tin.novakovic@comparethemarket.com>
2024-03-20 19:52:47 +0000

    DetailInfoScreen Representing personality score with an AnimatedLinearProgressIndicator

--- app/src/main/java/com/tinnovakovic/catcataloguer/presentation/SharedComponents.kt
+++ app/src/main/java/com/tinnovakovic/catcataloguer/presentation/SharedComponents.kt
@@ -2,34 +2,38 @@ package com.tinnovakovic.catcataloguer.presentation
 
 import android.widget.Toast
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
-import com.tinnovakovic.catcataloguer.presentation.detail.DetailContract
+import androidx.lifecycle.LifecycleOwner
 import com.tinnovakovic.catcataloguer.ui.theme.spacing
 
 @Composable
@@ -105,5 +109,40 @@ fun CatMediumTopAppBar(
             actionIconContentColor = MaterialTheme.colorScheme.onSurface
         )
 
+    )
+}
+
+@Composable
+fun AnimatedLinearProgressIndicator(
+    indicatorProgress: Float,
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+) {
+    var progress by rememberSaveable { mutableStateOf(0F) }
+    val progressAnimDuration = 1_500
+    val progressAnimation by animateFloatAsState(
+        targetValue = progress,
+        animationSpec = tween(durationMillis = progressAnimDuration, easing = FastOutSlowInEasing),
+    )
+    LinearProgressIndicator(
+        progress = { progressAnimation },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(MaterialTheme.spacing.small),
+        strokeCap = StrokeCap.Round
+
+    )
+    LaunchedEffect(lifecycleOwner) {
+        progress = indicatorProgress
+    }
+}
+
+@Composable
+fun SubTitle(
+    text: String
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.titleMedium,
+        modifier = Modifier.padding(top = MaterialTheme.spacing.large)
     )
 }
--- app/src/main/java/com/tinnovakovic/catcataloguer/presentation/detail/info/DetailInfoScreen.kt
+++ app/src/main/java/com/tinnovakovic/catcataloguer/presentation/detail/info/DetailInfoScreen.kt
@@ -1,10 +1,6 @@
 package com.tinnovakovic.catcataloguer.presentation.detail.info
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
@@ -17,24 +13,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material.icons.outlined.Close
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.tinnovakovic.catcataloguer.presentation.AnimatedLinearProgressIndicator
 import com.tinnovakovic.catcataloguer.presentation.ExpandingCard
+import com.tinnovakovic.catcataloguer.presentation.SubTitle
 import com.tinnovakovic.catcataloguer.presentation.detail.info.DetailInfoContract.*
 import com.tinnovakovic.catcataloguer.ui.theme.spacing
 
@@ -111,33 +103,17 @@ fun DetailScreenContent(
 
             item { SubTitle(text = "Personality Traits") }
             item {
-                FlowRow(
-                    horizontalArrangement = Arrangement.Absolute.SpaceBetween,
-                    maxItemsInEachRow = 4,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                {
-                    catDetail.personalityScores.forEach { personalityScore ->
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Text(
-                                text = personalityScore.personality,
-                                modifier = Modifier.fillMaxWidth(0.32f)
-                            )
-                            SuggestionChip(
-                                onClick = {},
-                                label = {
-                                    Text(
-                                        text = if (personalityScore.score == -1) {
-                                            "N/A"
-                                        } else {
-                                            "${personalityScore.score}/5"
-                                        }
-                                    )
-                                }
-                            )
-                        }
+                catDetail.personalityScores.forEach { personalityScore ->
+                    Column(
+                        modifier = Modifier.padding(top = MaterialTheme.spacing.small)
+                    ) {
+                        Text(
+                            text = personalityScore.personality,
+                            modifier = Modifier.padding(bottom = MaterialTheme.spacing.extraSmall)
+                        )
+                        AnimatedLinearProgressIndicator(
+                            indicatorProgress = personalityScore.score.toFloat().div(5)
+                        )
                     }
                 }
             }
@@ -181,15 +157,4 @@ fun DetailScreenContent(
             }
         }
     }
-}
-
-@Composable
-fun SubTitle(
-    text: String
-) {
-    Text(
-        text = text,
-        style = MaterialTheme.typography.titleMedium,
-        modifier = Modifier.padding(top = MaterialTheme.spacing.large)
-    )
 }
